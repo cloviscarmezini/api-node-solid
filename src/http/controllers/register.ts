5a13a79ad3aa071507858e784648b1bf0ec7f46f
cloviscarmezini <clovis.cjunior@telefonicabigdata.onmicrosoft.com>
2023-08-18 08:50:22 -0300

    create registerController

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,29 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { z } from 'zod';
+import { RegisterUseCase } from 'src/use-cases/register';
+import { PrismaUsersRepository } from 'src/repositories/prisma-users-repository';
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+	const registerBodySchema = z.object({
+		name: z.string(),
+		email: z.string().email(),
+		password: z.string().min(6)
+	});
+
+	const { name, email, password } = registerBodySchema.parse(request.body);
+
+	try {
+		const usersRepository = new PrismaUsersRepository();
+		const registerUseCase = new RegisterUseCase(usersRepository);
+		
+		await registerUseCase.execute({
+			name,
+			email,
+			password
+		});
+
+		return reply.status(201).send();
+	} catch(error) {
+		return reply.status(409).send();
+	}
+}
