3e4d1bfb7db9082e7d4748eece5aaced12cfd34d
Clovis Carmezini <cloviscarmezini14@gmail.com>
2023-08-30 00:05:54 -0300

    add profile controller

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,12 @@
 import { FastifyInstance } from 'fastify';
+
 import { register } from '@src/http/controllers/register';
-import { authenticate } from './controllers/authenticate';
+import { authenticate } from '@src/http/controllers/authenticate';
+import { profile } from '@src/http/controllers/profile';
 
 export async function appRoutes(app: FastifyInstance) {
 	app.post('/users', register);
 	app.post('/sessions', authenticate );
+	
+	app.get('/me', profile );
 }
