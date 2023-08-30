7ddd96345709825da105643a848569fc6cde52b2
Clovis Carmezini <cloviscarmezini14@gmail.com>
2023-08-30 00:05:16 -0300

    add jwt

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,10 +3,11 @@ import { z } from 'zod';
 
 const envSchema = z.object({
 	NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+	JWT_SECRET: z.string(),
 	PORT: z.coerce.number().default(3333),
 });
 
-const _env = envSchema.safeParse(envSchema);
+const _env = envSchema.safeParse(process.env);
 
 if(!_env.success) {
 	console.log('Invalid environment variables', _env.error.format());
