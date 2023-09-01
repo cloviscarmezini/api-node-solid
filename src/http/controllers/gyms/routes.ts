7ac091d1c61b422a52ed2aa99fcb07d47eec1873
Clovis Carmezini <cloviscarmezini14@gmail.com>
2023-09-01 00:16:09 -0300

    feat: created gym routes

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,7 +1,15 @@
 import { FastifyInstance } from 'fastify';
 
 import { verifyJWT } from '@src/http/middlewares/verify-jwt';
+import { nearby } from './nearby';
+import { search } from './search';
+import { create } from './create';
 
 export async function gymsRoutes(app: FastifyInstance) {
-	app.addHook('onRequest', verifyJWT);	
+	app.addHook('onRequest', verifyJWT);
+
+	app.post('/gyms/search', search);
+	app.post('/gyms/nearby', nearby );
+
+	app.post('/gyms', create);
 }
